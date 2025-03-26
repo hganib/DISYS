f7ef389eefe1ab7b56d47bbce9d44c9a0352f2ad
Haris Ganibegovic <ganibegovic8@gmail.com>
2025-03-26 14:22:08 +0100

    add springboot project folder

---
+++ springboot-api/university-app/src/main/java/at/technikum/universityapp/controller/LectureController.java
@@ -0,0 +1,27 @@
+package at.technikum.universityapp.controller;
+
+import at.technikum.universityapp.entity.Lecture;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+public class LectureController {
+
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "Hello World";
+    }
+
+    @GetMapping("/lectures")
+    public List<Lecture> all(@RequestParam LocalDateTime start) {
+        return List.of(
+                new Lecture("DISYS", "mkosmider", 5),
+                new Lecture("SLT", "mkosmider", 2)
+        );
+    }
+}
