e08ff6eaf35e6bfb30447ef4a4dab77c9365206c
Haris Ganibegovic <ganibegovic8@gmail.com>
2025-03-26 15:27:14 +0100

    add two api endpoints

--- springboot-api/Springboot-API/src/main/java/com/example/springbootapi/controller/EnergyController.java
+++ springboot-api/Springboot-API/src/main/java/com/example/springbootapi/controller/EnergyController.java
@@ -1,12 +1,22 @@
 package com.example.springbootapi.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EnergyController {
-    @GetMapping("/hello")
-    public String helloWorld() {
-        return "DISYS Hello World";
+    @GetMapping("/energy/current") //http://localhost:8080/energy/current
+    public String currentEnergy() {
+        int energy = 1234;
+        return String.format("Current Energy: %d KWh", energy);
     }
+
+    @GetMapping("/energy/historical") //http://localhost:8080/energy/historical?start=2025-01-01&end=2025-03-01
+    public String historicalEnergy(@RequestParam("start") String start,
+                                   @RequestParam("end") String end) {
+        int energy = 2346743;
+        return String.format("Historical Energy: %d from %s to %s", energy, start, end);
+    }
+
 }
